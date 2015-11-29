4e7972098b838c61ef5003292ddcbef602bc635f
andy <andypham2341@hotmail.com>
2015-11-29 03:09:59 -0500

    graph

--- androidproject/app/src/main/java/com/hackwestern/hashtune/hashtune/FullscreenActivity.java
+++ androidproject/app/src/main/java/com/hackwestern/hashtune/hashtune/FullscreenActivity.java
@@ -1,5 +1,9 @@
 package com.hackwestern.hashtune.hashtune;
 
+import android.support.v7.app.ActionBarActivity;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.audiofx.Visualizer;
 import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +11,8 @@ import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
+
+import com.example.androidaudiovisualizer.VisualizerView;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -36,6 +42,11 @@ public class FullscreenActivity extends AppCompatActivity {
     private View mControlsView2;
     private boolean mVisible;
 
+    VisualizerView mVisualizerView;
+
+    private MediaPlayer mMediaPlayer;
+    private Visualizer mVisualizer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,7 +54,7 @@ public class FullscreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fullscreen);
 
         mVisible = true;
-        mContentView = findViewById(R.id.fullscreen_content);
+        mContentView = findViewById(R.id.myvisualizerview);
 
 
         // Set up the user interaction to manually show or hide the system UI.
@@ -62,6 +73,8 @@ public class FullscreenActivity extends AppCompatActivity {
 
         //EXTRA
         findViewById(R.id.editText).setOnTouchListener(mDelayHideTouchListener);
+        mVisualizerView = (VisualizerView) findViewById(R.id.myvisualizerview);
+        initAudio();
     }
 
     @Override
@@ -126,6 +139,7 @@ public class FullscreenActivity extends AppCompatActivity {
         }
     };
 
+
     @SuppressLint("InlinedApi")
     private void show() {
         // Show the system bar
@@ -172,5 +186,58 @@ public class FullscreenActivity extends AppCompatActivity {
         String hashtag = input.getText().toString();
 
         // Do something in response to button click
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isFinishing() && mMediaPlayer != null) {
+            mVisualizer.release();
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+    }
+
+    private void initAudio() {
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        mMediaPlayer = MediaPlayer.create(this, R.raw.test);
+
+        setupVisualizerFxAndUI();
+        // Make sure the visualizer is enabled only when you actually want to
+        // receive data, and
+        // when it makes sense to receive data.
+        mVisualizer.setEnabled(true);
+        // When the stream ends, we don't need to collect any more data. We
+        // don't do this in
+        // setupVisualizerFxAndUI because we likely want to have more,
+        // non-Visualizer related code
+        // in this callback.
+        mMediaPlayer
+                .setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        mVisualizer.setEnabled(false);
+                    }
+                });
+        mMediaPlayer.start();
+
+    }
+
+    private void setupVisualizerFxAndUI() {
+
+        // Create the Visualizer object and attach it to our media player.
+        mVisualizer = new Visualizer(mMediaPlayer.getAudioSessionId());
+        mVisualizer.setCaptureSize(Visualizer.getCaptureSizeRange()[1]);
+        mVisualizer.setDataCaptureListener(
+                new Visualizer.OnDataCaptureListener() {
+                    public void onWaveFormDataCapture(Visualizer visualizer,
+                                                      byte[] bytes, int samplingRate) {
+                        mVisualizerView.updateVisualizer(bytes);
+                    }
+
+                    public void onFftDataCapture(Visualizer visualizer,
+                                                 byte[] bytes, int samplingRate) {
+                    }
+                }, Visualizer.getMaxCaptureRate() / 2, true, false);
     }
 }
